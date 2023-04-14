a5edfa3b36dfca4b8ff801adde25b6098b213897
Lyzev <lyzev.info@gmail.com>
2023-04-14 20:22:15 +0200

    Moved default events
    Signed-off-by: Lyzev <lyzev.info@gmail.com>

--- src/main/kotlin/dev.lyzev.api/events/EventManager.kt
+++ src/main/kotlin/dev.lyzev.api/events/EventManager.kt
@@ -57,3 +57,15 @@ object EventManager {
         }
     }
 }
+
+interface Event {
+    /**
+     * Trigger this event by invoking the EventManager with this instance
+     */
+    fun fire() = EventManager(this)
+}
+
+abstract class CancellableEvent : Event {
+    // Flag indicating whether this event has been cancelled or not
+    var isCancelled: Boolean = false
+}
