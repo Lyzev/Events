b78cf71e7e337d0916784fb678d3a5a2bdd41087
Lyzev <noreply@lyzev.dev>
2025-01-31 19:16:55 +0100

    fix(Copyright)

--- src/main/kotlin/dev.lyzev.api/events/EventManager.kt
+++ src/main/kotlin/dev.lyzev.api/events/EventManager.kt
@@ -1,7 +1,7 @@
 /*
  * This file is part of https://github.com/Lyzev/Events.
  *
- * Copyright (c) 2023-2025. Lyzev
+ * Copyright (c) 2025. Lyzev
  *
  * Events is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
